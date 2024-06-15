8e98ebceced1e9cef672bb91e0751131081d4e7f
clauds-macedo <yanalogic@gmail.com>
2024-06-15 17:42:25 -0300

    refactor: change margin to padding

--- src/global/components/container/styles.ts
+++ src/global/components/container/styles.ts
@@ -2,6 +2,6 @@ import { StatusBar } from 'react-native';
 import styled from 'styled-components/native';
 
 export const BaseContainer = styled.View`
-  margin-top: ${StatusBar.currentHeight}px;
+  padding-top: ${StatusBar.currentHeight}px;
   flex: 1;
 `;
